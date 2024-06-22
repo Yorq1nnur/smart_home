56bc7196bcfe677b847614165b91fa8073e733ab
Yorq1nnur <abduraimovyorqinjon4@gmail.com>
2024-06-22 13:07:03 +0500

    Simple works

--- lib/screens/tab/tab_box/tab_box_screen.dart
+++ lib/screens/tab/tab_box/tab_box_screen.dart
@@ -6,7 +6,10 @@ import 'package:smart_home/screens/tab/account/account_screen.dart';
 import 'package:smart_home/screens/tab/home/home_screen.dart';
 import 'package:smart_home/screens/tab/reports/reports_screen.dart';
 import 'package:smart_home/screens/tab/smart/smart_screen.dart';
+import 'package:smart_home/utils/app_colors.dart';
 import 'package:smart_home/utils/app_images.dart';
+import 'package:smart_home/utils/app_text_style.dart';
+import 'package:smart_home/utils/size.dart';
 
 class TabBoxScreen extends StatefulWidget {
   const TabBoxScreen({super.key});
@@ -31,57 +34,72 @@ class _TabBoxScreenState extends State<TabBoxScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: BlocBuilder<TabBoxBloc, TabBoxState>(builder: (context, state) {
-        return _screens[state.activeIndex];
-      }),
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: (v) {
-          context.read<TabBoxBloc>().add(
-                ChangeActiveIndexEvent(
-                  v,
+    return BlocBuilder<TabBoxBloc, TabBoxState>(
+      builder: (context, state) {
+        return Scaffold(
+          body: _screens[state.activeIndex],
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: state.activeIndex,
+            type: BottomNavigationBarType.fixed,
+            onTap: (v) {
+              context.read<TabBoxBloc>().add(
+                    ChangeActiveIndexEvent(
+                      v,
+                    ),
+                  );
+            },
+            unselectedItemColor: AppColors.c9E9E9E,
+            selectedItemColor: AppColors.c405FF2,
+            selectedLabelStyle: AppTextStyle.urbanistW700.copyWith(
+              fontSize: 10.w,
+              color: AppColors.c405FF2,
+            ),
+            unselectedLabelStyle: AppTextStyle.urbanistW500.copyWith(
+              fontSize: 10.w,
+              color: AppColors.c9E9E9E,
+            ),
+            backgroundColor: Colors.white,
+            items: [
+              BottomNavigationBarItem(
+                label: 'Home',
+                icon: SvgPicture.asset(
+                  AppImages.unActiveHome,
                 ),
-              );
-        },
-        items: [
-          BottomNavigationBarItem(
-            label: 'Home',
-            icon: SvgPicture.asset(
-              AppImages.unActiveHome,
-            ),
-            activeIcon: SvgPicture.asset(
-              AppImages.activeHome,
-            ),
+                activeIcon: SvgPicture.asset(
+                  AppImages.activeHome,
+                ),
+              ),
+              BottomNavigationBarItem(
+                label: 'Smart',
+                icon: SvgPicture.asset(
+                  AppImages.unActiveSmart,
+                ),
+                activeIcon: SvgPicture.asset(
+                  AppImages.activeSmart,
+                ),
+              ),
+              BottomNavigationBarItem(
+                label: 'Reports',
+                icon: SvgPicture.asset(
+                  AppImages.unActiveReport,
+                ),
+                activeIcon: SvgPicture.asset(
+                  AppImages.activeReport,
+                ),
+              ),
+              BottomNavigationBarItem(
+                label: 'Account',
+                icon: SvgPicture.asset(
+                  AppImages.unActiveAccount,
+                ),
+                activeIcon: SvgPicture.asset(
+                  AppImages.activeAccount,
+                ),
+              ),
+            ],
           ),
-          BottomNavigationBarItem(
-            label: 'Smart',
-            icon: SvgPicture.asset(
-              AppImages.unActiveSmart,
-            ),
-            activeIcon: SvgPicture.asset(
-              AppImages.activeSmart,
-            ),
-          ),
-          BottomNavigationBarItem(
-            label: 'Reports',
-            icon: SvgPicture.asset(
-              AppImages.unActiveReport,
-            ),
-            activeIcon: SvgPicture.asset(
-              AppImages.activeReport,
-            ),
-          ),
-          BottomNavigationBarItem(
-            label: 'Account',
-            icon: SvgPicture.asset(
-              AppImages.unActiveAccount,
-            ),
-            activeIcon: SvgPicture.asset(
-              AppImages.activeAccount,
-            ),
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
